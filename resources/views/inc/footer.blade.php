5ec2893e1756ab27b67d2beb1b3290a7eb39c8c6
Alwaleed <alwaleed.alageel@gmail.com>
2019-05-09 01:59:18 +0300

    Update footer.blade.php

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,7 +1,5 @@
 <div>
-        <p class="badge badge-primary text-wrap">footer @ {{date('Y   /  M  /  D  / H')}}</p>
-
-        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://soundcloud.com/mushari-alghamdi/sghchiqdwiuj">
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/341895981&color=%233c3c3c&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
 </iframe>
-
+<p class="badge badge-primary text-wrap">footer @ {{date('Y   /  M  /  D  / H')}}</p>
 </div>
