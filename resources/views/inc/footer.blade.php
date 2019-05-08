65f885861ebfe9506d1f26bdd7b265aaa7d84c56
Alwaleed <alwaleed.alageel@gmail.com>
2019-05-09 01:53:31 +0300

    Update footer.blade.php

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,3 +1,7 @@
 <div>
         <p class="badge badge-primary text-wrap">footer @ {{date('Y   /  M  /  D  / H')}}</p>
+
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://soundcloud.com/mushari-alghamdi/sghchiqdwiuj">
+</iframe>
+
 </div>
