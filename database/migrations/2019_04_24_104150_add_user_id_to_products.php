c7770c9c87bdd7e99787ad924aa4c4f99a49eec2
Alwaleed <alwaleed.alageel@gmail.com>
2019-05-14 03:05:22 +0300

    Update 2019_04_24_104150_add_user_id_to_products.php

--- database/migrations/2019_04_24_104150_add_user_id_to_products.php
+++ database/migrations/2019_04_24_104150_add_user_id_to_products.php
@@ -14,7 +14,7 @@ class AddUserIdToProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->bigIncrements('user_id');
         });
     }
 
