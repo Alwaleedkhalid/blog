c0be25af0f7eeb4c1668bbfa141eb22c30f02769
Alwaleed <alwaleed.alageel@gmail.com>
2019-05-18 05:09:14 +0300

    start poinet

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -26,7 +26,7 @@
                       <a class="nav-link" href="/services">Services</a>
                 </li>
               <li class="nav-item">
-                <a class="nav-link" href="/insert_product">Insert</a>
+                <a class="nav-link" href="/insert_product">Products</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="/form">Forms</a>
