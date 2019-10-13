545a196d498839b815627964029796ddc612d3c2
Alwaleed <alwaleed.alageel@gmail.com>
2019-10-14 01:23:49 +0300

    last update

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,6 +1,6 @@
-{{-- <div>
+<div>
 <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/341895981&color=%233c3c3c&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
-</div>  --}}
+</div> 
 
     <p class="badge badge-secondary text-wrap float-right">{{date('Y   -  M  -  D  -  H')}}</p>
     <i class="fab fa-twitter float-left badge-light"> - </i>
