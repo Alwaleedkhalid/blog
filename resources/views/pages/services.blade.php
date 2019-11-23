6279717633d75f5f80907f9f112488a9bc3acd8d
Alwaleed <alwaleed.alageel@gmail.com>
2019-11-23 23:17:09 +0300

    Update services.blade.php

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -4,11 +4,11 @@
 
         <h1>{{$title}}</h1>
         <hr>
-        @if(count($services > 0))
+        {{-- @if(count($services > 0)) --}}
             <ul class="list-group">
                 @foreach($services as $service)
                     <li class="list-group-item">{{$service}}</li>
                 @endforeach
             </ul>
-        @endif
+        {{-- @endif --}}
     @endsection
