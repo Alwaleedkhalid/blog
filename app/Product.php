5bef0b9586ad89731bab8831ae8ad1ebd256c81b
Alwaleed <alwaleed.alageel@gmail.com>
2020-05-18 23:22:09 +0300

    relations

--- app/Product.php
+++ app/Product.php
@@ -2,14 +2,18 @@
 
 namespace App;
 
+use App\Review;
 use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
-{
+{   
     protected $guarded = ['fn','ln'];
     public $timestamps = false;
 
-
-        
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
     }
+        
+}
 
