40b267b94cd3c71825f7404869f40efc7c67221c
Alwaleed <alwaleed.alageel@gmail.com>
2020-05-20 20:39:28 +0300

    destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -104,11 +104,11 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  App\Product $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($product)
     {
-        //
+        return $product;
     }
 }
