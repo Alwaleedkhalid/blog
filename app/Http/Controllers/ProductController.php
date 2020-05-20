bfede51a4eb8fb0bb9a01c219f28bfb35563a2de
Alwaleed <alwaleed.alageel@gmail.com>
2020-05-20 05:28:24 +0300

    updated product
    ..

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
+use Symfony\Component\HttpFoundation\Response;
 use App\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function __constrect()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +42,21 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product;
+
+        $product->name = $request->name;
+        $product->detial = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+        
+
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
@@ -56,10 +75,10 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  App\Product $product
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($product)
     {
         //
     }
@@ -68,12 +87,18 @@ class ProductController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  App\Product $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request,Product $product)
     {
-        //
+        $request['detial'] = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
+
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
     }
 
     /**
--- app/Product.php
+++ app/Product.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class product extends Model
 {   
     protected $guarded = ['fn','ln'];
+    protected $fillable = [
+        'name','detial','stock','price','discount'
+    ];
     public $timestamps = false;
 
     public function reviews()
