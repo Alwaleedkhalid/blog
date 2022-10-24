13562f4f62d9b8fcec0b651cfe939a1780f775d9
Alwaleedkhalid <alwaleed.alageel@gmail.com>
2022-10-24 14:18:53 +0300

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -4,6 +4,6 @@
         <h1>{{$title}}</h1>
         <hr>
         <div class="shadow-lg p-3 mb-5 bg-white rounded">Web Desiner</div>
-        <a href="/about" class=" btn-secondary">defult btn Bootstrap</a>
+        <!-- <a href="/about" class=" btn-secondary">defult btn Bootstrap</a> -->
     
     @endsection
